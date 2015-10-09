f77b36bbb5ae43f385fc1ae66392b2074cd36eac
Julien Vallini <vallinij@gmail.com>
2015-10-09 21:13:39 +0200

    Configure user validation datable

---
+++ src/AppBundle/Resources/public/js/user_validation_table.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#user-validation-table').DataTable({
+        responsive: true,
+        searching: false,
+        lengthChange: false,
+        info: false,
+        pageLength: 5
+    });
+});
